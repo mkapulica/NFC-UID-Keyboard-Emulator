ba0283f1d3f4dd8352ed2fb038e0e6bb254626e4
mislav <mislav.kapulica@vern.hr>
2023-08-24 16:01:08 +0200

    feat: UI responsiveness

--- UIDtoKeyboard/frmMain.Designer.vb
+++ UIDtoKeyboard/frmMain.Designer.vb
@@ -30,51 +30,62 @@ Partial Class frmMain
         Me.rbOriginal = New System.Windows.Forms.RadioButton()
         Me.rbReversed = New System.Windows.Forms.RadioButton()
         Me.chkSendEnter = New System.Windows.Forms.CheckBox()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'cbxReaderList
         '
+        Me.cbxReaderList.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.cbxReaderList.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
         Me.cbxReaderList.FormattingEnabled = True
-        Me.cbxReaderList.Location = New System.Drawing.Point(25, 22)
+        Me.cbxReaderList.Location = New System.Drawing.Point(3, 3)
         Me.cbxReaderList.Name = "cbxReaderList"
-        Me.cbxReaderList.Size = New System.Drawing.Size(400, 26)
+        Me.cbxReaderList.Size = New System.Drawing.Size(327, 26)
         Me.cbxReaderList.TabIndex = 0
         '
         'btnRefreshReader
         '
+        Me.btnRefreshReader.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnRefreshReader.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
-        Me.btnRefreshReader.Location = New System.Drawing.Point(431, 22)
+        Me.btnRefreshReader.Location = New System.Drawing.Point(336, 3)
         Me.btnRefreshReader.Name = "btnRefreshReader"
-        Me.btnRefreshReader.Size = New System.Drawing.Size(87, 27)
+        Me.btnRefreshReader.Size = New System.Drawing.Size(106, 27)
         Me.btnRefreshReader.TabIndex = 2
         Me.btnRefreshReader.Text = "Refresh"
         Me.btnRefreshReader.UseVisualStyleBackColor = True
         '
         'btnStartMonitor
         '
+        Me.btnStartMonitor.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnStartMonitor.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
-        Me.btnStartMonitor.Location = New System.Drawing.Point(25, 112)
+        Me.btnStartMonitor.Location = New System.Drawing.Point(12, 112)
         Me.btnStartMonitor.Name = "btnStartMonitor"
-        Me.btnStartMonitor.Size = New System.Drawing.Size(496, 32)
+        Me.btnStartMonitor.Size = New System.Drawing.Size(445, 32)
         Me.btnStartMonitor.TabIndex = 3
         Me.btnStartMonitor.Text = "Start Monitor"
         Me.btnStartMonitor.UseVisualStyleBackColor = True
         '
         'txtInputSpace
         '
+        Me.txtInputSpace.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtInputSpace.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
-        Me.txtInputSpace.Location = New System.Drawing.Point(25, 159)
+        Me.txtInputSpace.Location = New System.Drawing.Point(12, 161)
         Me.txtInputSpace.Multiline = True
         Me.txtInputSpace.Name = "txtInputSpace"
-        Me.txtInputSpace.Size = New System.Drawing.Size(496, 206)
+        Me.txtInputSpace.Size = New System.Drawing.Size(445, 161)
         Me.txtInputSpace.TabIndex = 4
         '
         'lblReadingMode
         '
         Me.lblReadingMode.AutoSize = True
         Me.lblReadingMode.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
-        Me.lblReadingMode.Location = New System.Drawing.Point(22, 59)
+        Me.lblReadingMode.Location = New System.Drawing.Point(9, 59)
         Me.lblReadingMode.Name = "lblReadingMode"
         Me.lblReadingMode.Size = New System.Drawing.Size(83, 18)
         Me.lblReadingMode.TabIndex = 9
@@ -85,7 +96,7 @@ Partial Class frmMain
         Me.rbOriginal.AutoSize = True
         Me.rbOriginal.Checked = True
         Me.rbOriginal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-        Me.rbOriginal.Location = New System.Drawing.Point(111, 59)
+        Me.rbOriginal.Location = New System.Drawing.Point(98, 59)
         Me.rbOriginal.Name = "rbOriginal"
         Me.rbOriginal.Size = New System.Drawing.Size(68, 19)
         Me.rbOriginal.TabIndex = 10
@@ -97,7 +108,7 @@ Partial Class frmMain
         '
         Me.rbReversed.AutoSize = True
         Me.rbReversed.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-        Me.rbReversed.Location = New System.Drawing.Point(185, 59)
+        Me.rbReversed.Location = New System.Drawing.Point(172, 59)
         Me.rbReversed.Name = "rbReversed"
         Me.rbReversed.Size = New System.Drawing.Size(77, 19)
         Me.rbReversed.TabIndex = 11
@@ -108,28 +119,46 @@ Partial Class frmMain
         '
         Me.chkSendEnter.AutoSize = True
         Me.chkSendEnter.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-        Me.chkSendEnter.Location = New System.Drawing.Point(25, 84)
+        Me.chkSendEnter.Location = New System.Drawing.Point(12, 84)
         Me.chkSendEnter.Name = "chkSendEnter"
         Me.chkSendEnter.Size = New System.Drawing.Size(100, 22)
         Me.chkSendEnter.TabIndex = 12
         Me.chkSendEnter.Text = "Send Enter"
         Me.chkSendEnter.UseVisualStyleBackColor = True
         '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.AutoSize = True
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.cbxReaderList, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnRefreshReader, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 12)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(445, 33)
+        Me.TableLayoutPanel1.TabIndex = 13
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(540, 395)
+        Me.ClientSize = New System.Drawing.Size(469, 338)
+        Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Controls.Add(Me.chkSendEnter)
         Me.Controls.Add(Me.rbReversed)
         Me.Controls.Add(Me.rbOriginal)
         Me.Controls.Add(Me.lblReadingMode)
         Me.Controls.Add(Me.txtInputSpace)
         Me.Controls.Add(Me.btnStartMonitor)
-        Me.Controls.Add(Me.btnRefreshReader)
-        Me.Controls.Add(Me.cbxReaderList)
+        Me.MinimumSize = New System.Drawing.Size(333, 304)
         Me.Name = "frmMain"
         Me.Text = "UIDtoKeyboard"
+        Me.TableLayoutPanel1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -143,4 +172,5 @@ Partial Class frmMain
     Friend WithEvents rbOriginal As RadioButton
     Friend WithEvents rbReversed As RadioButton
     Friend WithEvents chkSendEnter As CheckBox
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
 End Class
